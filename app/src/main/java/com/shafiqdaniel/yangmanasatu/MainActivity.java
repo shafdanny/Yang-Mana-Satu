a104502a0170a33aedd27def602b75f6b4202743
Shafiq Daniel <shafdanny@gmail.com>
2016-04-10 20:19:53 +0200

    Add change activity when button is pushed

--- app/src/main/java/com/shafiqdaniel/yangmanasatu/MainActivity.java
+++ app/src/main/java/com/shafiqdaniel/yangmanasatu/MainActivity.java
@@ -1,13 +1,28 @@
 package com.shafiqdaniel.yangmanasatu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityChangeIntent = new Intent(MainActivity.this, QuestionActivity.class);
+                startActivity(activityChangeIntent);
+            }
+        });
     }
+
+
 }
