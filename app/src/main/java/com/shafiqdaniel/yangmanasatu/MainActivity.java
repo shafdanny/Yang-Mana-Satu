b4d230714de4c3adcc82fae4997b74448990568f
Shafiq Daniel <shafdanny@gmail.com>
2016-04-11 12:59:24 +0200

    Reset high score working

--- app/src/main/java/com/shafiqdaniel/yangmanasatu/MainActivity.java
+++ app/src/main/java/com/shafiqdaniel/yangmanasatu/MainActivity.java
@@ -1,5 +1,7 @@
 package com.shafiqdaniel.yangmanasatu;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -12,8 +14,11 @@ public class MainActivity extends AppCompatActivity {
 
     Button startBtn;
     Button aboutBtn;
+    Button resetScoreBtn;
     TextView topScoreTextView;
     int topScore;
+    AlertDialog.Builder builder;
+    DialogInterface.OnClickListener dialogClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +26,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         startBtn = (Button) findViewById(R.id.start_button);
         aboutBtn = (Button) findViewById(R.id.about_button);
+        resetScoreBtn = (Button) findViewById(R.id.reset_score_button);
+
         topScoreTextView = (TextView) findViewById(R.id.score_textview_main);
+
+         dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        resetHighScore();
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        //No button clicked
+                        break;
+                }
+            }
+        };
+
+        builder = new AlertDialog.Builder(this);
 
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,10 +63,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        resetScoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                builder.setMessage(getString(R.string.reset_score_prompt_message))
+                        .setPositiveButton(getString(R.string.yes), dialogClickListener)
+                        .setNegativeButton(getString(R.string.no), dialogClickListener)
+                        .show();
+            }
+        });
+
         SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.score_file_key), this.MODE_PRIVATE);
         int defaultScore = 0;
         topScore = sharedPref.getInt(getString(R.string.saved_high_score), defaultScore);
-        topScoreTextView.append("" + topScore);
+        topScoreTextView.setText(getString(R.string.high_score_text) + " " + topScore);
+    }
+
+    private void resetHighScore() {
+        SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.score_file_key), this.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(getString(R.string.saved_high_score), 0);
+        editor.commit();
+        topScore = sharedPref.getInt(getString(R.string.saved_high_score), 0);
+        topScoreTextView.setText(getString(R.string.high_score_text) + " " + topScore);
     }
 
 
