19965c3ed54c12c3366f719a15146d1674b4a6af
Shafiq Daniel <shafdanny@gmail.com>
2016-04-11 03:16:29 +0200

    Add random answer picker

--- app/src/main/java/com/shafiqdaniel/yangmanasatu/QuestionActivity.java
+++ app/src/main/java/com/shafiqdaniel/yangmanasatu/QuestionActivity.java
@@ -5,10 +5,16 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.PopupWindow;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by shafiq on 10/04/16.
@@ -22,10 +28,14 @@ public class QuestionActivity extends AppCompatActivity {
     int timerMax;
     int score;
 
+    String[] option = {"Selangor", "Kedah", "Kelantan", "Johor", "Pahang" };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_answer);
+
+        initAnswerButton();
 
         timerMax = getResources().getInteger(R.integer.timerMax);
 
@@ -35,6 +45,37 @@ public class QuestionActivity extends AppCompatActivity {
         scoreTextView.setText(Integer.toString(score));
         progressBar.setProgress(timerMax);
 
+    }
+
+    private void initAnswerButton() {
+        Button ansBtn1 = (Button) findViewById(R.id.choice1);
+        Button ansBtn2 = (Button) findViewById(R.id.choice2);
+        Button ansBtn3 = (Button) findViewById(R.id.choice3);
+        Button ansBtn4 = (Button) findViewById(R.id.choice4);
+
+        String[] pickedAnswerOptions = pickNRandom(option, 4);
+
+        ansBtn1.setText(pickedAnswerOptions[0]);
+        ansBtn2.setText(pickedAnswerOptions[1]);
+        ansBtn3.setText(pickedAnswerOptions[2]);
+        ansBtn4.setText(pickedAnswerOptions[3]);
+
+
+    }
+
+    public static String[] pickNRandom(String[] array, int n) {
+
+        List<String> list = new ArrayList<String>(array.length);
+        for (String i : array)
+            list.add(i);
+        Collections.shuffle(list);
+
+        String[] answer = new String[n];
+        for (int i = 0; i < n; i++)
+            answer[i] = list.get(i);
+        Arrays.sort(answer);
+
+        return answer;
     }
 
     @Override
